01981cb3494113b1a98d2ca3a608260d7a217ebf
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-06-24 08:29:37 -0700

    getting ready for chirality.
     * add spatial-arrangements to molecule class
    
     * start to flesh out tetrahedral-center class

--- chemicl.lisp
+++ chemicl.lisp
@@ -1,6 +1,6 @@
 ;;; file: chemicl.lisp
 ;;;
-;;; Copyright (c) 2008-2009 Cyrus Harmon (ch-lisp@bobobeach.com)
+;;; Copyright (c) 2008-2010 Cyrus Harmon (ch-lisp@bobobeach.com)
 ;;; All rights reserved.
 ;;;
 ;;; Redistribution and use in source and binary forms prohibited.
@@ -74,7 +74,8 @@ symbol containing an element symbol (such as Fe or :fe for Iron)."
 (defclass molecule (graph:simple-edge-list-graph)
   ((name :initarg :name :accessor name :initform nil)
    (atom-name-hash :accessor atom-name-hash
-                   :initform (make-hash-table :test 'equal)))
+                   :initform (make-hash-table :test 'equal))
+   (spatial-arrangements :accessor spatial-arrangements :initform nil))
   (:documentation "A class for representing molecules."))
 
 (defun make-molecule (&rest args)
@@ -313,6 +314,9 @@ aromatic or not."
 
 (defclass spatial-arrangement () ())
 
-(defclass tetrahedral-center
-    (spatial-arrangement) ())
+(defclass tetrahedral-center (spatial-arrangement)
+  ((chiral-atom :initarg :chiral-atom :accessor chiral-atom)
+   (neighbors :initarg :neighbors :accessor neighbors)
+   ;; possible orientations are :clockwise and :anticlockwise
+   (orientation :initarg :orientation :accessor :orientation)))
 
